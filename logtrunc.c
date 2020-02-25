f83861fb13138ac557169308f5954ecf2c0e0d6d
Maxime Daniel <root@maxux.net>
2020-02-25 12:11:10 +0100

    Service and watcher improvments
    - Service can be stopped/started before truncating
    - Service manager can be set at compile time
    - If watched file is removed, watching is re-installed
    - If file doesn't exists when trying to watch, waiting indefinitively

--- logtrunc.c
+++ logtrunc.c
@@ -6,6 +6,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define SRVMAIN    "zinit"
+#define MAXSIZEMB  1536
+
 #ifndef RELEASE
     #define debug(...) { printf(__VA_ARGS__); }
 #else
@@ -21,55 +24,91 @@ void warnp(char *str) {
     perror(str);
 }
 
-int postprocess(char *process) {
+int watchfile(int inotfd, char *filename) {
+    int watchfd;
+
+    debug("[+] trying to watch logfile\n");
+
+    while((watchfd = inotify_add_watch(inotfd, filename, IN_MODIFY)) < 0) {
+        warnp(filename);
+        usleep(100000);
+    }
+
+    return 0;
+}
+
+int srvproc(char *action, char *service) {
+    int status;
     pid_t pp = fork();
 
     if(pp < 0) {
-        warnp(process);
+        warnp("fork");
         return 1;
     }
 
     if(pp == 0) {
-        debug("[+] executing post process handler\n");
+        debug("[+] dependency: %s %s %s\n", SRVMAIN, action, service);
 
-        char *fargv[] = {process, process, 0};
-        execv(process, fargv);
+        char *fargv[] = {SRVMAIN, action, service, 0};
+        execv(SRVMAIN, fargv);
         diep("execl");
     }
 
-    int status;
     debug("[+] waiting for post-process handler to finish\n");
     waitpid(pp, &status, 0);
 
     return 0;
 }
 
+int srvstop(char *service) {
+    debug("[+] stopping dependant service: %s\n", service);
+    return srvproc("stop", service);
+}
+
+int srvstart(char *service) {
+    debug("[+] starting dependant services: %s\n", service);
+    return srvproc("start", service);
+}
+
 int main(int argc, char *argv[]) {
-    char *filename = argv[1];
-    char *postproc = "/tmp/postproc.sh";
-    int inotfd, watchd;
+    char *filename = NULL;
+    char *service = NULL;
+    int inotfd;
     struct inotify_event event;
     struct stat stbuf;
-    off_t maxsize = 5 * 1024; // 1024 * 1024 * 5;
+    off_t maxsize = MAXSIZEMB * 1024 * 1024;
 
     debug("[+] initializing logtrunc\n");
 
-    if(argc != 2) {
-        fprintf(stderr, "Usage: %s filename\n", argv[0]);
+    if(argc < 2) {
+        fprintf(stderr, "Usage: %s <filename> [service]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
+    filename = argv[1];
     debug("[+] monitoring logfile: %s\n", filename);
+
+    if(argc > 2) {
+        service = argv[2];
+        debug("[+] service to restart: %s\n", service);
+    }
 
     if((inotfd = inotify_init()) < 0)
         diep("inotify_init");
 
-    if((watchd = inotify_add_watch(inotfd, filename, IN_MODIFY)) < 0)
-        diep("inotify_add_watch");
+    // registering file to watch
+    watchfile(inotfd, filename);
 
-    debug("[+] monitoring for maxsize: %lu\n", maxsize);
+    debug("[+] monitoring for maxsize: %lu bytes (%.2f GB)\n", maxsize, (maxsize / (1024.0 * 1024 * 1024)));
 
     while(read(inotfd, &event, sizeof(event)) > 0) {
+        if(event.mask & IN_IGNORED) {
+            debug("[-] watching ignored, file was probably removed\n");
+            debug("[-] re-watching file if we can\n");
+            watchfile(inotfd, filename);
+            continue;
+        }
+
         if(stat(filename, &stbuf) != 0) {
             warnp(filename);
             continue;
@@ -81,12 +120,17 @@ int main(int argc, char *argv[]) {
         if(stbuf.st_size > maxsize) {
             debug("[+] maximum size reached, truncating\n");
 
+            // stopping dependant service
+            if(service)
+                srvstop(service);
+
             // truncate the file
             if(truncate(filename, 0) < 0)
                 warnp(filename);
 
-            // execute post process
-            postprocess(postproc);
+            // restarting dependant service
+            if(service)
+                srvstart(service);
         }
     }
 
